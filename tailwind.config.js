9c2bbcd71f7841c17ad85ac861896f2464b2167f
khanghoang782 <jakekgg2002@gmail.com>
2023-12-15 07:48:21 +0700

    order list added

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,13 @@ module.exports = {
   content: ["./dist/*.{html,js,php}"],
   theme: {
     extend: {},
+    maxWidth: {
+      '1/4': '25%',
+      '1/2': '50%',
+      '3/4': '75%',
+     }
   },
   plugins: [],
 }
+
 
